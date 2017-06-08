5610c55ebadd1b9cd6f5f79185ef94e9122fdfe7
Hugo Benício <hbobenicio@gmail.com>
2017-06-08 07:48:10 -0300

    Adding basic thread usage example

---
+++ basic-thread-usage/src/main.rs
@@ -0,0 +1,23 @@
+use std::thread;
+
+fn main() {
+    let nthreads = 10;
+
+    // This won't compile at first, because the compiler
+    // doesn't know about vectors type. When it sees
+    // the .push() method been called below, it will
+    // infer the vectors type and our code become
+    // compilable
+    let mut threads = vec![];
+
+    for _ in 0..nthreads {
+        threads.push(thread::spawn(|| {
+            println!("Hello, World!");
+        }));
+    }
+
+    for t in threads {
+        t.join().unwrap();
+    }
+}
+
