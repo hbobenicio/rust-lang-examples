361f5a4d71073767d18bc2e523a0dff901503803
Hugo Benício <hbobenicio@gmail.com>
2018-09-02 22:10:33 -0300

    Rendering the score text

--- ggez-examples/snake/src/main.rs
+++ ggez-examples/snake/src/main.rs
@@ -1,78 +1,28 @@
 extern crate ggez;
 
+mod constants;
 mod direction;
+mod food;
+mod score;
 mod snake;
+mod game;
 
-use ggez::Context;
-use ggez::graphics::{self, Color, Point2};
-use ggez::event;
-use snake::Snake;
-use direction::Direction;
-
-type GameResult = ggez::GameResult<()>;
-
-pub struct Game {
-    snake: Snake
-}
-
-impl Game {
-    pub fn new() -> Game {
-        let velocity = 1.0;
-        let initial_pos = Point2::new(50.0, 50.0);
-        let snake = Snake::new(initial_pos, velocity, Color::from_rgb(0, 255, 0));
-
-        Game {
-            snake
-        }
-    }
-}
-
-impl ggez::event::EventHandler for Game {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        self.snake.update();
-        
-        Ok(())
-    }
-
-    fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        graphics::clear(ctx);
-
-        self.snake.draw(ctx)?;
-
-        graphics::present(ctx);
-        Ok(())
-    }
-
-    fn key_down_event(&mut self, _ctx: &mut Context, keycode: event::Keycode, _keymod: event::Mod, _repeat: bool) {
-        match keycode {
-            event::Keycode::Up | 
-            event::Keycode::Down |
-            event::Keycode::Left |
-            event::Keycode::Right => {
-                let new_direction = Direction::from(keycode);
-                self.snake.change_direction(new_direction);
-                // TODO Tratar o caso em mudar de mais de 2 direções antes do update
-                // (Adicionar numa lista de movimentos, talvez...)
-            },
-            _ => {},
-        }
-    }
-}
+use game::Game;
 
 fn main() {
     let mut conf = ggez::conf::Conf::new();
     conf.window_mode = ggez::conf::WindowMode::default()
-        .dimensions(400, 300);
+        .dimensions(constants::WINDOW_WIDTH as u32, constants::WINDOW_HEIGHT as u32);
     conf.window_setup = ggez::conf::WindowSetup::default()
         .title("Snake Game");
 
-    let mut ctx = Context::load_from_conf("Snake", "Hugo Benício", conf)
+    let mut ctx = ggez::Context::load_from_conf("Snake", "Hugo Benício", conf)
         .expect("Unable to create game context");
 
-    graphics::set_background_color(&mut ctx, graphics::BLACK);
+    ggez::graphics::set_background_color(&mut ctx, ggez::graphics::BLACK);
 
     let mut game = Game::new();
-    if let Err(e) = event::run(&mut ctx, &mut game) {
-        println!("Error: {}", e);
+    if let Err(e) = ggez::event::run(&mut ctx, &mut game) {
+        eprintln!("Error: {}", e);
     };
 }
--- ggez-examples/snake/src/snake.rs
+++ ggez-examples/snake/src/snake.rs
@@ -1,11 +1,11 @@
 use std::mem;
-use ggez::graphics::{self, Rect, Color, Point2};
-use ggez::Context;
-use ggez::GameResult;
-use direction::{self, Direction};
 
-const BLOCK_SIZE: f32 = 25.0;
-const UPDATE_DELAY: f32 = 25.0;
+use ggez::{self, Context};
+use ggez::graphics::{self, Rect, Color, Point2};
+
+use constants;
+use direction::{self, Direction};
+use game::GameResult;
 
 #[derive(Debug)]
 pub struct Snake {
@@ -20,7 +20,7 @@ impl Snake {
     pub fn new(initial_pos: Point2, velocity: f32, color: Color) -> Self {
         let segments = vec![
             initial_pos,
-            Point2::new(initial_pos.coords.x - BLOCK_SIZE, initial_pos.coords.y)
+            Point2::new(initial_pos.coords.x - constants::BLOCK_SIZE, initial_pos.coords.y)
         ];
 
         Snake {
@@ -32,38 +32,6 @@ impl Snake {
         }
     }
 
-    pub fn draw(&self, ctx: &mut Context) -> GameResult<()> {
-
-        for segment in &self.segments {
-            let x = segment.coords.x;
-            let y = segment.coords.y;
-            
-            graphics::set_color(ctx, self.color)?;
-            graphics::rectangle(ctx, graphics::DrawMode::Fill, Rect::new(x, y, BLOCK_SIZE, BLOCK_SIZE))?;
-            graphics::set_color(ctx, Color::from_rgb(0, 100, 0))?;
-            graphics::rectangle(ctx, graphics::DrawMode::Line(2.0), Rect::new(x, y, BLOCK_SIZE, BLOCK_SIZE))?;
-        }
-
-        Ok(())
-    }
-
-    pub fn update(&mut self) {
-        self.update_tick += self.velocity;
-
-        if self.update_tick < UPDATE_DELAY {
-            return;
-        }
-
-        let mut next_pos = self.next_pos();
-
-        for segment in &mut self.segments {
-            mem::swap(&mut next_pos.x, &mut segment.coords.x);
-            mem::swap(&mut next_pos.y, &mut segment.coords.y);
-        }
-
-        self.update_tick = 0.0;
-    }
-
     fn next_pos(&self) -> Point2 {
         let x: f32;
         let y: f32;
@@ -73,26 +41,26 @@ impl Snake {
         match self.direction {
             Direction::Up => {
                 x = head.x;
-                y = if head.y - BLOCK_SIZE < 0.0 {
-                    300.0 - BLOCK_SIZE
+                y = if head.y - constants::BLOCK_SIZE < 0.0 {
+                    constants::WINDOW_HEIGHT - constants::BLOCK_SIZE
                 } else {
-                    head.y - BLOCK_SIZE
+                    head.y - constants::BLOCK_SIZE
                 };
             },
             Direction::Down => {
                 x = head.x;
-                y = (head.y + BLOCK_SIZE) % 300.0;
+                y = (head.y + constants::BLOCK_SIZE) % constants::WINDOW_HEIGHT;
             },
             Direction::Left => {
-                x = if head.x - BLOCK_SIZE < 0.0 {
-                    400.0 - BLOCK_SIZE
+                x = if head.x - constants::BLOCK_SIZE < 0.0 {
+                    constants::WINDOW_WIDTH - constants::BLOCK_SIZE
                 } else {
-                    head.x - BLOCK_SIZE
+                    head.x - constants::BLOCK_SIZE
                 };
                 y = head.y;
             },
             Direction::Right => {
-                x = (head.x + BLOCK_SIZE) % 400.0;
+                x = (head.x + constants::BLOCK_SIZE) % constants::WINDOW_WIDTH;
                 y = head.y;
             }
         };
@@ -104,5 +72,39 @@ impl Snake {
         if !direction::opposite(self.direction, new_direction) {
             self.direction = new_direction;
         }
+    }
+}
+
+impl ggez::event::EventHandler for Snake {
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        for segment in &self.segments {
+            let x = segment.coords.x;
+            let y = segment.coords.y;
+            
+            graphics::set_color(ctx, self.color)?;
+            graphics::rectangle(ctx, graphics::DrawMode::Fill, Rect::new(x, y, constants::BLOCK_SIZE, constants::BLOCK_SIZE))?;
+            graphics::set_color(ctx, Color::from_rgb(0, 100, 0))?;
+            graphics::rectangle(ctx, graphics::DrawMode::Line(2.0), Rect::new(x, y, constants::BLOCK_SIZE, constants::BLOCK_SIZE))?;
+        }
+
+        Ok(())
+    }
+
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        self.update_tick += self.velocity;
+
+        if self.update_tick < constants::UPDATE_DELAY {
+            return Ok(());
+        }
+
+        let mut next_pos = self.next_pos();
+
+        for segment in &mut self.segments {
+            mem::swap(&mut next_pos.x, &mut segment.coords.x);
+            mem::swap(&mut next_pos.y, &mut segment.coords.y);
+        }
+
+        self.update_tick = 0.0;
+        Ok(())
     }
 }
