597c5c42f5d5384899a5da5d84961c63f25f39e2
Hugo Benicio <hbobenicio@gmail.com>
2018-10-02 14:42:59 -0300

    Little code organizations

---
+++ rust-gtk-hello-world/src/app/mod.rs
@@ -0,0 +1,75 @@
+use gtk::{prelude::*, Button, ToolButton, Window, Builder};
+
+#[derive(Debug)]
+pub struct App {
+  main_window: Window,
+  add_toolbutton: ToolButton,
+  remove_toolbutton: ToolButton,
+  start_button: Button,
+}
+
+impl App {
+    pub fn new() -> Result<App, String> {
+      gtk::init()
+          .map_err(|e| format!("Failed to initialize GTK: {}", e))?;
+
+      let glade_src: &str = include_str!("../resources/ui/main_window.glade");
+      let builder: Builder = gtk::Builder::new_from_string(glade_src);
+
+      let main_window: Window = get_object_from_builder(&builder, "main_window")?;
+      let add_toolbutton: ToolButton = get_object_from_builder(&builder, "add_toolbutton")?;
+      let remove_toolbutton: ToolButton = get_object_from_builder(&builder, "remove_toolbutton")?;
+      let start_button: Button = get_object_from_builder(&builder, "start_button")?;
+
+      let app = App {
+        main_window,
+        add_toolbutton,
+        remove_toolbutton,
+        start_button
+      };
+
+      app.connect_signals();
+      app.show();
+
+      Ok(app)
+    }
+
+    pub fn start(&self) -> Result<(), String> {
+      gtk::main();
+
+      Ok(())
+    }
+
+    fn connect_signals(&self) {
+      self.main_window.connect_delete_event(|_win, _event: &gdk::Event| {
+          gtk::main_quit();
+          Inhibit(false)
+      });
+      self.add_toolbutton.connect_clicked(|_b| {
+          println!("Add button clicked");
+      });
+      self.remove_toolbutton.connect_clicked(|_b| {
+          println!("Remove button clicked");
+      });
+      self.start_button.connect_clicked(|_b| {
+          println!("Start button clicked");
+      });
+    }
+
+    fn show(&self) {
+      self.main_window.show_all();
+    }
+
+}
+
+fn get_object_from_builder<O>(builder: &Builder, object_name: &str) -> Result<O, String>
+where
+    O: gtk::IsA<gtk::Object>
+{
+    builder
+      .get_object(object_name)
+      .ok_or(
+        format!("Error while getting object with id {} from builder", object_name)
+      )
+}
+
--- rust-gtk-hello-world/src/main.rs
+++ rust-gtk-hello-world/src/main.rs
@@ -1,36 +1,20 @@
 extern crate gtk;
 extern crate gdk;
 
-use gtk::{prelude::*};
-
-// use gtk::{Button, Window, WindowType};
+mod app;
 
 fn main() {
-    if let Err(reason) = gtk::init() {
-        eprintln!("Failed to initialize GTK: {}", reason);
+    let app = app::App::new();
+
+    if let Err(reason) = app {
+        eprintln!("Application initialization error: {}", reason);
+        std::process::exit(1);
+    };
+
+    let app = app.unwrap();
+
+    if let Err(reason) = app.start() {
+        eprintln!("Application Error: {}", reason);
         std::process::exit(1);
     }
-
-    let glade_src = include_str!("resources/ui/main_window.glade");
-    let builder = gtk::Builder::new_from_string(glade_src);
-    let window: gtk::Window = builder.get_object("main_window")
-        .expect("Não foi possível obter objeto 'main_window' da interface main_window.glade");
-
-    // let window = Window::new(WindowType::Toplevel);
-    // window.set_title("First GTK+ Program");
-    // window.set_default_size(350, 70);
-    // let button = Button::new_with_label("Click me!");
-    // window.add(&button);
-    window.show_all();
-    
-    window.connect_delete_event(|_w: &gtk::Window, _event: &gdk::Event| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
-    //
-    // button.connect_clicked(|_| {
-    //     println!("Clicked!");
-    // });
-
-    gtk::main();
 }
