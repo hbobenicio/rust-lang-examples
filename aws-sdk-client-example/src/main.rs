439da7ef3c32a48fbde7f3bb1c640342d5b57b41
Hugo Benicio <hbobenicio@gmail.com>
2018-10-10 08:56:08 -0300

    Adding aws sdk with rusoto crate example

--- aws-sdk-client-example/src/main.rs
+++ aws-sdk-client-example/src/main.rs
@@ -1,3 +1,11 @@
+/// Exemplo de Client AWS usando o crate Rusoto.
+/// 
+/// ## Ideias
+/// - Transformar este exemplo em uma CLI usando o `clap` e o `colored`
+/// - Usar interface semelhante ao awscli da Amazon (mas apenas para o S3)
+/// - Usar .env no diretório corrente, ao invés do `~/.aws/credentials`
+/// - Opções de deploy: deb/snap/docker
+/// 
 extern crate rusoto_core;
 extern crate rusoto_credential;
 extern crate rusoto_s3;
@@ -10,7 +18,7 @@ fn main() {
     let credentials_provider = rusoto_credential::EnvironmentProvider::default();
     let region = rusoto_core::Region::Custom {
         name: String::from("us-west-2"),
-        endpoint: String::from("https://storagegw.estaleiro.serpro.gov.br")
+        endpoint: String::from("")
     };
 
     let s3: S3Client = rusoto_s3::S3Client::new_with(
